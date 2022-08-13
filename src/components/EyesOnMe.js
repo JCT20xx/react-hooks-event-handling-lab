bec6db7a7374cb5348dc8a3e51225c4e474b39c3
Juan Toro <jct20xx@gmail.com>
2022-08-13 01:41:56 -0400

    commit tree f2aec47d59bbdbc12c8c20df5deccf88a261310c

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,22 @@
-// Code EyesOnMe Component Here
+import react from "react";
+
+function EyesOnMe(){
+    function focus(event){
+        console.log('Good!')
+    }
+    function blur(event){
+        console.log('Hey! Eyes on me!')
+    }
+    return(
+        <div>
+            <button 
+            onFocus={focus} 
+            onBlur={blur}>
+                Eyes on me
+                </button>
+        </div>
+    )
+}
+
+
+export default EyesOnMe
